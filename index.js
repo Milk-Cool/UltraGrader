12c3fe208aef821588d6e995e48cf1fb366d0689
Milk-Cool <milan-pl@mail.ru>
2024-05-15 12:39:41 +0300

    only removing after an hour

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.post("/api/upload", (req, res) => {
     setTimeout(pathToRemove => {
         if(DEBUG) console.log("Removing file", pathToRemove);
         fs.unlinkSync(pathToRemove);
-    }, 5000, path);
+    }, 60 * 60 * 1000, path);
     res.status(200).end(uuid);
 });
 
