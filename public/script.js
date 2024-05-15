8aa038df055ec3571940ed80b17aa55da1d1e954
Milk-Cool <milan-pl@mail.ru>
2024-05-15 17:26:31 +0300

    fix mutliplt submissions

--- public/script.js
+++ public/script.js
@@ -6,26 +6,30 @@ const toggleCriteriaVisibility = () => {
     document.querySelector("#criteria").classList.toggle("hidden");
 };
 
-const upload = e => new Promise((resolve, reject) => {
-    const files = e.target.files;
-    if(!files || files.length == 0) reject();
-    for(const file of files) {
-        const fr = new FileReader();
-        fr.onload = async e2 => {
-            if(e2.target.readyState != FileReader.DONE) return;
-            const f = await fetch("/api/upload", {
-                "method": "POST",
-                "body": e2.target.result,
-                "headers": {
-                    "Content-Type": "application/octet-stream"
-                }
-            });
-            if(f.status != 200) reject();
-            resolve(f.text());
-        };
-        fr.readAsArrayBuffer(file);
-    }
+const uploadOne = file => new Promise((resolve, reject) => {
+    const fr = new FileReader();
+    fr.onload = async e2 => {
+        if(e2.target.readyState != FileReader.DONE) return;
+        const f = await fetch("/api/upload", {
+            "method": "POST",
+            "body": e2.target.result,
+            "headers": {
+                "Content-Type": "application/octet-stream"
+            }
+        });
+        if(f.status != 200) reject();
+        resolve(f.text());
+    };
+    fr.readAsArrayBuffer(file);
 });
+const upload = async e => {
+    const files = e.target.files;
+    let o = [];
+    if(!files || files.length == 0) reject();
+    for(const file of files)
+        o.push(await uploadOne(file));
+    return o.join(",");
+};
 
 const removeSubmission = uuid => {
     document.querySelector(`[data-uuid='${uuid}']`).remove();
@@ -100,26 +104,30 @@ const uploadCriteria = async e => {
 };
 document.querySelector("#criteria-upload").addEventListener("change", uploadCriteria);
 const uploadSubmission = async e => {
-    const uuid = await upload(e);
-    submissions.push(uuid);
-
-    const submission = document.createElement("div");
-    submission.classList.add("submission");
-    submission.setAttribute("data-uuid", uuid);
-    const name = document.createElement("p");
-    name.classList.add("submission-name");
-    name.innerText = e.target.files[0]?.name;
-    name.addEventListener("click", () => {
-        const files = `${criteria},${uuid}`;
-        analyzeDisplay(files);
-    })
-    submission.appendChild(name);
-    const remove = document.createElement("md-icon");
-    remove.classList.add("submission-remove");
-    remove.innerText = "delete";
-    remove.addEventListener("click", () => removeSubmission(uuid));
-    submission.appendChild(remove);
-    document.querySelector("#submissions").appendChild(submission);
+    const uuids = await upload(e);
+    for(const i of uuids.split(","))
+        submissions.push(i);
+    let i = 0;
+    for(const file of e.target.files) {
+        const submission = document.createElement("div");
+        submission.classList.add("submission");
+        submission.setAttribute("data-uuid", uuids[i]);
+        const name = document.createElement("p");
+        name.classList.add("submission-name");
+        name.innerText = file.name;
+        name.addEventListener("click", () => {
+            const files = `${criteria},${uuids[i]}`;
+            analyzeDisplay(files);
+        })
+        submission.appendChild(name);
+        const remove = document.createElement("md-icon");
+        remove.classList.add("submission-remove");
+        remove.innerText = "delete";
+        remove.addEventListener("click", () => removeSubmission(uuids[i]));
+        submission.appendChild(remove);
+        document.querySelector("#submissions").appendChild(submission);
+        i++;
+    }
 };
 document.querySelector("#submission-upload").addEventListener("change", uploadSubmission);
 
